490a1ae4fc9b5a56437f027c99009ab5e9c55927
MaciekZ23 <maciekz232002@o2.pl>
2025-05-12 22:16:20 +0200

    Poprawki, navbar, home, user-table

--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -1,6 +1,5 @@
-// Serwis do obslugi zapytan do API uzytkownikow
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 
@@ -8,30 +7,32 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 
-export class UserService 
-{
+export class UserService {
   private apiUrl = 'https://reqres.in/api/users';
 
-  constructor(private http: HttpClient) {}
-  
-  getUsers(): Observable<any[]> 
-  {
-    return this.http.get<any>(this.apiUrl).pipe(map(res => res.data));
+  constructor(private http: HttpClient) { }
+
+  private getHeaders() {
+    return new HttpHeaders({
+      'x-api-key': 'reqres-free-v1'
+    });
   }
 
-  addUser(user: any): Observable<any> 
-  {
-    return this.http.post<any>(this.apiUrl, user);
+  getUsers(): Observable<any[]> {
+    return this.http.get<any>(this.apiUrl, { headers: this.getHeaders() }).pipe(
+      map(res => res.data)
+    );
   }
 
-  updateUser(user: any): Observable<any>
-  {
-    return this.http.put<any>(`${this.apiUrl}/${user.id}`, user);
+  addUser(user: any): Observable<any> {
+    return this.http.post<any>(this.apiUrl, user, { headers: this.getHeaders() });
   }
 
-  deleteUser(id: number): Observable<any>
-  {
-    return this.http.delete<any>(`${this.apiUrl}/${id}`);
+  updateUser(user: any): Observable<any> {
+    return this.http.put<any>(`${this.apiUrl}/${user.id}`, user, { headers: this.getHeaders() });
   }
-  
+
+  deleteUser(id: number): Observable<any> {
+    return this.http.delete<any>(`${this.apiUrl}/${id}`, { headers: this.getHeaders() });
+  }
 }
--- frontend/src/app/user-form/user-form.component.ts
+++ frontend/src/app/user-form/user-form.component.ts
@@ -1,6 +1,7 @@
-import { Component, EventEmitter, Output, Input } from '@angular/core';
+import { Component, EventEmitter, Output, Input, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+declare const bootstrap: any;
 
 @Component({
   selector: 'app-user-form',
@@ -9,32 +10,37 @@ import { FormsModule } from '@angular/forms';
   styleUrls: ['./user-form.component.scss']
 })
 
-export class UserFormComponent {
+export class UserFormComponent implements AfterViewInit {
   @Input() user: any = null;
   @Output() close = new EventEmitter<void>();
   @Output() save = new EventEmitter<any>();
+
+  @ViewChild('modal') modalElement!: ElementRef;
+  private modalInstance: any;
 
   firstName = '';
   lastName = '';
   email = '';
 
-  ngOnInit() 
-  {
-    if (this.user) 
-      {
+  ngOnInit() {
+    if (this.user) {
       this.firstName = this.user.first_name;
       this.lastName = this.user.last_name;
       this.email = this.user.email;
     }
   }
 
-  onClose() 
-  {
+  ngAfterViewInit() {
+    this.modalInstance = new bootstrap.Modal(this.modalElement.nativeElement);
+    this.modalInstance.show();
+  }
+
+  onClose() {
+    this.modalInstance.hide();
     this.close.emit();
   }
 
-  onSave() 
-  {
+  onSave() {
     if (!this.firstName || !this.lastName || !this.email) return;
     const user = {
       ...this.user,
